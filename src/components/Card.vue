6495d48cb7680f640141bd2eb1d1e49f00af2e3a
MOHIT RANA <awesomemohit20@gmail.com>
2024-10-28 20:01:02 +0530

    Defined different components for the pages

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script>
+
+    export default {
+        props : {
+            bg : {
+                type : String,
+                default : 'bg-gray-100',
+            },
+        }
+    }
+
+</script>
+
+<template>
+    <div :class="`${bg} p-6 rounded-lg shadow-md`">
+        <slot></slot>
+    </div>
+</template>
