e245eceaa78f8c87b13bf0ab4fdd3fb468008013
MOHIT RANA <awesomemohit20@gmail.com>
2024-10-28 20:01:34 +0530

    Different views for the Applications

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+    import Hero from '@/components/Hero.vue';
+    import HomeCards from '@/components/HomeCards.vue';
+    import JobListings from '@/components/JobListings.vue';
+
+    export default{
+        components: {
+            Hero,
+            HomeCards,
+            JobListings
+        }
+    }
+</script>
+
+<template>
+    <Hero />
+    <HomeCards />
+    <JobListings :limit="3" :showButton="true" />
+</template>
