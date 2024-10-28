36acb783d1a549f4ed9d91c966df2619c6f81198
MOHIT RANA <awesomemohit20@gmail.com>
2024-10-28 20:01:49 +0530

    Render the application

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/Hero.vue';
+  import HomeCards from '@/components/HomeCards.vue'
+  import { RouterView } from 'vue-router';
+
+  export default {
+    components: {
+      Navbar,
+      Hero,
+      HomeCards,
+    }
+  }
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
